11b31e37ac8454fbd85d4a69c2c5beb74e07a815
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-07-31 15:08:58 +0200

    got pictures, kid messing with me

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,27 @@
-import "./footer.css"
+import "./footer.css";
 import { Link } from "react-router-dom";
 const Footer = () => {
-    return (
+  return (
     <div className="footer-container">
-        <br/>
-        <nav className="link-container">
-        <Link to ="/"><p>Hem</p></Link>
-        <Link to ="/omoss"><p>Om oss</p></Link>
-        <Link to ="/meny"><p>Meny</p></Link>
-        <Link to ="/kontakt"><p>Kontakt / Events</p></Link>
-        </nav>
-        <br/>
-        <p>© 2024 Glädjekällans foodtruck</p>
+      <br />
+      <nav className="link-container">
+        <Link to="/">
+          <p>Hem</p>
+        </Link>
+        <Link to="/omoss">
+          <p>Om oss</p>
+        </Link>
+        <Link to="/meny">
+          <p>Meny</p>
+        </Link>
+        <Link to="/kontakt">
+          <p>Kontakt / Events / <br/>Företagsevents</p>
+        </Link>
+      </nav>
+      <br />
+      <p>© 2024 Glädjekällans foodtruck</p>
     </div>
-    );
-}
+  );
+};
 
 export default Footer;
